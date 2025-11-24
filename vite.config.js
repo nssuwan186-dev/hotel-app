dc4b634c3c18e6cdb659c8fc907d6b21d1a3f62b
root <root@localhost.localdomain>
2025-11-24 00:08:44 +0000

    Update Vite config for correct build path

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/hotel-app/', // Set this to your repository name
+  root: './public', // Explicitly tell Vite where to find index.html
+  build: {
+    outDir: '../dist', // Output to a 'dist' folder outside the public folder
+  },
 })
